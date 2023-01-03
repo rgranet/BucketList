629085d8294a0f405ba9798a57a959d9ed6dfc73
Ruben Granet <ruben.granet@icloud.com>
2023-01-03 10:41:50 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Ruben Granet on 03/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
